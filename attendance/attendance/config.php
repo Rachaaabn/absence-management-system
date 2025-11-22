7255995564f61c471b48244d1604df425aa0035c
Rachaaabn <rachabounoua29@gmail.com>
2025-11-22 21:09:20 +0100

    Delete attendance directory

--- attendance/attendance/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configuration file for database connection
-
-$DB_HOST = "localhost";      // database server
-$DB_USER = "root";           // username (change if needed)
-$DB_PASS = "";               // password (often empty in localhost)
-$DB_NAME = "attendance_db";  // your database name
-?>
